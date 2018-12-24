70492a0cce54d4aa5e5605857b9f327f48e1e68e
wondermacros <wondermacros@jiivonen-laptop>
2018-12-24 15:49:25 +0200

    Add W_REF_VOID_PTR.

---
+++ wondermacros/compiler/ref_void_ptr.h
@@ -0,0 +1,33 @@
+/* (C) Copyright 2017 J.P. Iivonen <wondermacros@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/***
+ *** Name:  W_REF_VOID_PTR
+ *** Proto: W_REF_VOID_PTR(ptr,offset)
+ *** Arg:   ptr     a pointer       A pointer to be referenced by an offset.
+ *** Arg:   offset  an offset       An offset in bytes.
+ *** Description: Use W_REF_VOID_PTR to reference a void pointer using an offset in bytes.
+ ***/
+#define PP_REF_VOID_PTR(ptr,offset) \
+    ((void*)((char*) (ptr) + (offset)))
