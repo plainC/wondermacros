6ffc32c7a677e501018a99291f783b0f29985e0f
wondermacros <wondermacros@jiivonen-laptop>
2018-12-24 15:43:35 +0200

    Add W_UNUSED and add comments.

--- wondermacros/char/to_char.h
+++ wondermacros/char/to_char.h
@@ -1,4 +1,38 @@
+/* (C) Copyright 2017 J.P. Iivonen <wondermacros@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/***
+ *** Name:  W_TO_CHAR
+ *** Proto: W_TO_CHAR(ch)
+ *** Arg:   ch   a character       A character to be converted to ASCII.
+ *** Description: Use W_TO_CHAR to convert a single character to a C character.
+ *** Note:  ch must be a valid character to be used in an identifer, i.g. [a-zA-Z_0-9].
+ *** Example: 'char str[] = { W_TO_CHAR(a), W_TO_CHAR(b), 0 };' expands to
+ ***          'char str[] = { 'a', 'b', 0 };'
+ ***/
 #define W_TO_CHAR(ch) _W_TO_CHAR(ch)
+
 #define _W_TO_CHAR(ch) _W_TO_CHAR_ ## ch
 
 #define _W_TO_CHAR_0 '0'
