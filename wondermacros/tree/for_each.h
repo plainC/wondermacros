46f36234c001294d70e8f6b681aa6321e31e6b10
wondermacros <wondermacros@jiivonen-laptop>
2019-01-28 15:39:44 +0200

    Add W_TREE_FOR_EACH_PREORDER.

---
+++ wondermacros/tree/for_each.h
@@ -0,0 +1,96 @@
+/* (C) Copyright 2019 J.P. Iivonen <wondermacros@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef __W_TREE_FOR_EACH_H
+#define __W_TREE_FOR_EACH_H
+
+#include <wondermacros/meta/after.h>
+#include <wondermacros/meta/before.h>
+#include <wondermacros/meta/declare.h>
+#include <wondermacros/meta/id.h>
+#include <wondermacros/meta/cat.h>
+#include <wondermacros/array/dynamic_stack.h>
+
+
+#ifndef W_TREE_NEXT
+# define W_TREE_NEXT(tree,ix) ((tree)->next[ix])
+#endif
+
+#ifndef W_TREE_GET_DEGREE
+# include <wondermacros/array/array_get_size.h>
+# define W_TREE_GET_DEGREE(tree) W_ARRAY_GET_SIZE((tree)->next)
+#endif
+
+#ifndef W_REVERSED
+# define W_REVERSED 0
+#endif
+
+
+#define W_TREE_NEXT_LEFTMOST(tree)                                             \
+    W_TREE_NEXT(tree,0)
+
+#define W_TREE_NEXT_RIGHTMOST(tree)                                            \
+    W_TREE_NEXT(tree,TREE_GET_DEGREE(tree)-1)
+
+#define W_TREE_FOR_EACH_IMMEDIATE(T,node,tree)                                 \
+    W_DECLARE(10, T* node)                                                     \
+    for (int W_CAT(node,_ix)=0; W_CAT(node,_ix) < W_TREE_GET_DEGREE(node)      \
+         && (node = W_TREE_NEXT(tree,W_CAT(node,_ix)),1); W_CAT(node,_ix)++)   \
+        if (node == NULL)                                                      \
+            ;                                                                  \
+        else
+
+#define W_TREE_FOR_EACH_IMMEDIATE_REVERSED(T,node,tree)                        \
+    W_DECLARE(10, T* node)                                                     \
+    for (int W_CAT(node,_ix) = W_TREE_GET_DEGREE(node)-1;                      \
+         W_CAT(node,_ix) >= 0                                                  \
+         && (node = W_TREE_NEXT(tree, W_CAT(node,_ix)),1);                     \
+      W_CAT(node,_ix)--)                                                       \
+        if (node == NULL)                                                      \
+            ;                                                                  \
+        else
+
+#define W_TREE_FOR_EACH_PREORDER(T,node,self)                                  \
+    W_DECLARE(1, T** W_ID(stack) = NULL)                                       \
+    W_DECLARE(2, T* node)                                                      \
+    W_DECLARE(22, T* W_ID(root) = (self))                                      \
+    W_BEFORE(3,                                                                \
+        W_DYNAMIC_STACK_PUSH(W_ID(stack), W_ID(root) );                        \
+    )                                                                          \
+    if (W_ID(root) == NULL)                                                    \
+        ;                                                                      \
+    else                                                                       \
+        while (!W_DYNAMIC_STACK_IS_EMPTY(W_ID(stack)))                         \
+            W_BEFORE(4, (node) = W_DYNAMIC_STACK_POP(W_ID(stack)))             \
+            W_AFTER(5,                                                         \
+                if (node)                                                      \
+                    BOOST_PP_IF(W_REVERSED,                                    \
+                        W_TREE_FOR_EACH_IMMEDIATE_REVERSED,                    \
+                        W_TREE_FOR_EACH_IMMEDIATE)                             \
+                    (T,W_ID(tmp), node)                                        \
+                        W_DYNAMIC_STACK_PUSH(W_ID(stack), W_ID(tmp));          \
+            )                                                                  \
+    /**/
+
+#endif
+
