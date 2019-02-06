1c1312277106e90ac9f35ad3571ca262ec73f655
wondermacros <wondermacros@jiivonen-laptop>
2019-02-06 23:10:14 +0200

    Removed TAGS.

--- examples/objects/html/dom_string_class.h
+++ examples/objects/html/dom_string_class.h
@@ -1,7 +1,5 @@
 #define CLASS dom_string
 
-#define TAGS 1
-
 #define dom_string__define                    \
     /* Inherits */                            \
     html__define                              \
