d4bd44fc07614be8621871ad612ddb37a22e9171
J.P. Iivonen <wondermacros@yahoo.com>
2019-06-24 23:01:52 +0300

    Add Object.c.

---
+++ wondermacros/core/Object.c
@@ -0,0 +1,25 @@
+#define W_IS_PUBLIC 0
+#include "oo_api.h"
+#include "Object__class.h"
+#include "oo_expand.h"
+
+
+Object* Object__clone(Object* self)
+{
+}
+
+void Object__free(Object* self)
+{
+}
+
+#define W_IS_PUBLIC 0
+struct W_CLASS_STRUCT_NAME(ObjectMeta) W_CLASS_INSTANCE_NAME(ObjectMeta) = {
+  .klass = NULL,
+  .name = "ObjectMeta",
+  .kind = CLASS_KIND_META,
+};
+
+int main()
+{
+}
+
